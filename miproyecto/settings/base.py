a002cb64c3e446fa693c2a3e161f6a15994772d0
lucasexrojas <109619163+lucasexrojas@users.noreply.github.com>
2022-07-26 12:04:59 -0300

    cambios segunda parte
    agregue los cambios que solicitaba en la segunda parte del video, lo que no modifique fue lo de 'local' en manage porque en mi proyecto me daba error asique no me quise arriesgar en este

--- miproyecto/settings/base.py
+++ miproyecto/settings/base.py
@@ -10,7 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
+from tracemalloc import StatisticDiff
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'miproyecto.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(os.path.dirname(BASE_DIR),'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -105,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Argentina/Buenos_Aires'
 
 USE_I18N = True
 
@@ -116,6 +118,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS= (
+    os.path.join(os.path.dirname(BASE_DIR),'static'),
+    )
+
+MEDIA_URL ='/media/'
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR),'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
